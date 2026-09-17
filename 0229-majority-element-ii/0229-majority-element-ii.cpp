294beacbcbb63f8d5c0349559fb692817da564d7
hardikvikasjain <hardikvikasjain@gmail.com>
2026-09-17 21:43:03 +0530

    Time: 0 ms (100%), Space: 23.6 MB (90.02%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
@@ -26,6 +26,61 @@ public:
         if(count > n/3) {
             ans.push_back(nums[n-1]);
         }
+
+        return ans;
+    }
+};*/
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+
+        // Find possible candidates
+        for(int i = 0; i < n; i++) {
+
+            if(nums[i] == candidate1) {
+                count1++;
+            }
+            else if(nums[i] == candidate2) {
+                count2++;
+            }
+            else if(count1 == 0) {
+                candidate1 = nums[i];
+                count1 = 1;
+            }
+            else if(count2 == 0) {
+                candidate2 = nums[i];
+                count2 = 1;
+            }
+            else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Verify
+        count1 = 0;
+        count2 = 0;
+
+        for(int num : nums) {
+            if(num == candidate1)
+                count1++;
+
+            if(num == candidate2)
+                count2++;
+        }
+
+        vector<int> ans;
+
+        if(count1 > n/3)
+            ans.push_back(candidate1);
+
+        if(count2 > n/3 && candidate2 != candidate1)
+            ans.push_back(candidate2);
 
         return ans;
     }
